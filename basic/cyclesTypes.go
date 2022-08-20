fcc52c501ddc3c71ac76da8751625910325ba362
Alex Christian <alexchristianqr@gmail.com>
2022-08-20 02:15:58 -0500

    [updated] ciclos

---
+++ basic/cyclesTypes.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+func main() {
+	const counter = 10
+
+	// Ciclo for tradicional
+	for index := 1; index <= counter; index++ {
+		fmt.Println("El indice es:", index)
+	}
+
+	fmt.Printf("-- break -- \n")
+
+	// Ciclo for while
+	index := 1
+	for index <= counter {
+		fmt.Println("El indice es:", index)
+		index++
+	}
+
+	fmt.Printf("-- break -- \n")
+
+	// Ciclo for forever
+	index2 := 1
+	for {
+		fmt.Println("El indice es:", index2)
+		index2++
+		if index2 > counter {
+			break
+		}
+	}
+
+	fmt.Printf("-- break -- \n")
+
+	// Ejemplo de ciclo for con indice decreciente
+	for index := 10; index > 0; index-- {
+		fmt.Println("El indice es:", index)
+	}
+}
